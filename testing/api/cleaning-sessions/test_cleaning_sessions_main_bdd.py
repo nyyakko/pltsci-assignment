6cf852ec4d15741d1b9dcd7f51d098734bbe9483
nyyakko <nyakonyns@gmail.com>
2026-02-28 20:34:22 -0300

    load api base url from .env

--- testing/api/cleaning-sessions/test_cleaning_sessions_main_bdd.py
+++ testing/api/cleaning-sessions/test_cleaning_sessions_main_bdd.py
@@ -1,14 +1,16 @@
+from dotenv import load_dotenv
 from pytest_bdd import scenario, scenarios, given, when, step, then, parsers
 import json
+import os
 import requests
 
-BASE = "http://localhost:8080/v1"
+load_dotenv()
 
 scenarios('features/cleaning_sessions.feature')
 
 @step(parsers.parse("I send \"{method}\" request with body to \"{path}\":"), target_fixture="response")
 def when_i_send_request_with_body_to(method, path, docstring):
-    response = requests.post(f"{BASE}/cleaning-sessions", headers={"Content-Type": "application/json"}, data=docstring, timeout=5)
+    response = requests.post(f"{os.getenv("API_BASE_URL")}/cleaning-sessions", headers={"Content-Type": "application/json"}, data=docstring, timeout=5)
     return response
 
 @step(parsers.parse("the response status code should be {code:d}"))
--- testing/api/cleaning-sessions/test_cleaning_sessions_main_fuzz.py
+++ testing/api/cleaning-sessions/test_cleaning_sessions_main_fuzz.py
@@ -1,10 +1,12 @@
+from dotenv import load_dotenv
 from hypothesis import given, settings, strategies as st, Verbosity
 from hypothesis.strategies import text, integers, lists
 import json
+import os
 import requests
 import time
 
-BASE = "http://localhost:8080/v1"
+load_dotenv()
 
 json_body = st.fixed_dictionaries({
     "roomSize": st.lists(st.integers(min_value=1, max_value=1000), min_size=2, max_size=2),
@@ -16,6 +18,6 @@ json_body = st.fixed_dictionaries({
 @settings(max_examples=500, deadline=None)
 @given(body=json_body)
 def test_cleaning_sessions(body):
-    response = requests.post(f"{BASE}/cleaning-sessions", headers={"Content-Type": "application/json"}, data=json.dumps(body), timeout=5)
+    response = requests.post(f"{os.getenv("API_BASE_URL")}/cleaning-sessions", headers={"Content-Type": "application/json"}, data=json.dumps(body), timeout=5)
     assert not (500 <= response.status_code <= 599)
     # time.sleep(0.5)
