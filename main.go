9610e2c376b6fcaa382e0655957cb35694575e26
nyyakko <nyakonyns@gmail.com>
2026-03-08 18:56:14 -0300

    add health-check endpoint
    * fix wrong request method

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"assignment/api/hoover"
 	"assignment/middleware"
+	"assignment/utils"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+func checkHealth(w http.ResponseWriter, r *http.Request) *http_utils.HttpError {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(struct { Status string `json:"status"` }{ Status: "ok" })
+	return nil
+}
+
 func main() {
 	mux := http.NewServeMux()
 
+	mux.Handle("GET /v1/health", middleware.ErrorHandlerMiddleware(checkHealth))
 	mux.Handle("POST /v1/cleaning-sessions", middleware.ErrorHandlerMiddleware(hoover.Controller.CleaningSessions))
 
 	fmt.Println("Server is running on port 8080")
